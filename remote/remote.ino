900d859b70d29f58b7447529d7d1f55164bc0bd1
JT Raber <jmann240raber@gmail.com>
2022-05-03 21:50:02 -0400

    it moves, not great, but it does

--- remote/remote.ino
+++ remote/remote.ino
@@ -1,3 +1,6 @@
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
 #define JX A8
 #define JY A9
 
@@ -9,48 +12,131 @@
 #define SW6 59
 #define SW7 58
 
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+int bt_msg_stride = 100;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(38400, SERIAL_8E2);
   Serial1.begin(38400, SERIAL_8E2);
 
   pinMode(JX, INPUT);
   pinMode(JY, INPUT);
 
-  pinMode(SW1, INPUT);
-  pinMode(SW2, INPUT);
-  pinMode(SW3, INPUT);
-  pinMode(SW4, INPUT);
-  pinMode(SW5, INPUT);
-  pinMode(SW6, INPUT);
-  pinMode(SW7, INPUT);
+  pinMode(SW1, INPUT_PULLUP);
+  pinMode(SW2, INPUT_PULLUP);
+  pinMode(SW3, INPUT_PULLUP);
+  pinMode(SW4, INPUT_PULLUP);
+  pinMode(SW5, INPUT_PULLUP);
+  pinMode(SW6, INPUT_PULLUP);
+  pinMode(SW7, INPUT_PULLUP);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+  
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+
+  delay(1000);
+}
+
+double right_coeff = 1;
+double left_coeff = 1.8;
+
+long int current_time;
+long int last_time;
+
+int forward, leftward;
+
+void printVars() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("time: ");
+  display.println(current_time);
+  display.print("right_coeff: ");
+  display.println(right_coeff);
+  display.print("left_coeff: ");
+  display.println(left_coeff);
+  display.print(digitalRead(SW1));
+  display.print(" ");
+  display.println(digitalRead(SW3));
+  display.print(" ");
+  display.println(digitalRead(SW4));
+  display.print(" ");
+  display.println(digitalRead(SW5));
+  display.display();
 }
 
 void loop() {
-  int forward = analogRead(JY);
-  int leftward = analogRead(JX);
+  current_time = millis();
+  
+  forward = analogRead(JY);
+  leftward = analogRead(JX);
 
-  if (forward > (1024 - 256)) {
-    Serial1.write(0b1111);
-    Serial.println("forward");
-    last_pos = "forward";
-  } else if (forward < 256) {
-    Serial1.write(0b0101);
-    Serial.println("backward");
-    last_pos = "backward";
-  } else if (leftward > (1024 - 256)) {
-    Serial1.write(0b1101);
-    Serial.println("leftward");
-    last_pos = "leftward";
-  } else if (leftward < 256) {
-    Serial1.write(0b0111);
-    Serial.println("rightward");
-    last_pos = "rightward";
-  } else if (forward >= 256 && forward <= (1024 - 256) && leftward >= 256 && leftward <= (1024 - 256)) {
-    Serial1.write(0b0000);
-    Serial.println("stop");
-    last_pos = "stop";
+  if (digitalRead(SW4) == 0) {
+    if (digitalRead(SW1) == 0) {
+      left_coeff += 0.01;
+    }
+    if (digitalRead(SW3) == 0) {
+      right_coeff += 0.01;
+    }
   }
 
-  delay(200);
+  if (digitalRead(SW5) == 0) {
+    if (digitalRead(SW1) == 0) {
+      left_coeff -= 0.01;
+    }
+    if (digitalRead(SW3) == 0) {
+      right_coeff -= 0.01;
+    }
+  }
+
+  if (current_time >= last_time + bt_msg_stride) {
+    if (forward > (1024 - 256)) {
+      Serial1.write(2);
+      Serial1.print("S GO F");
+      Serial1.write(3);
+      Serial.println("forward");
+    } else if (forward < 256) {
+      Serial1.write(2);
+      Serial1.print("S GO B");
+      Serial1.write(3);
+      Serial.println("backward");
+    } else if (leftward > (1024 - 256)) {
+      Serial1.write(2);
+      Serial1.print("S GO L");
+      Serial1.write(3);
+      Serial.println("leftward");
+    } else if (leftward < 256) {
+      Serial1.write(2);
+      Serial1.print("S GO R");
+      Serial1.write(3);
+      Serial.println("rightward");
+    } else if (forward >= 256 && forward <= (1024 - 256) && leftward >= 256 && leftward <= (1024 - 256)) {
+      Serial1.write(2);
+      Serial1.print("S GO S");
+      Serial1.write(3);
+      Serial.println("stop");
+    }
+
+    Serial1.write(2);
+    Serial1.print("S LC ");
+    Serial1.print(left_coeff);
+    Serial1.write(3);
+
+    Serial1.write(2);
+    Serial1.print("S RC ");
+    Serial1.print(right_coeff);
+    Serial1.write(3);
+
+    last_time = current_time;
+  }
+
+  printVars();
+
+  delay(10);
 }
--- robot/robot.ino
+++ robot/robot.ino
@@ -1,6 +1,5 @@
-#include <SoftwareSerial.h>
 #include <Wire.h>
-#include <Adafruit_SSD1306.h> //Display
+#include <Adafruit_SSD1306.h>
 
 #define PIN_MOTOR_PWM_LEFT    9
 #define PIN_MOTOR_IN0_LEFT    7
@@ -18,16 +17,8 @@
 
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
-// SoftwareSerial mySerial(2, 3); // RX, TX
-
-int getBit(byte n, int b) {
-  return (n & (1 << b)) >> b;
-}
-
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(38400, SERIAL_8E2);
-  // mySerial.begin(38400, SERIAL_8E2);
   
   pinMode(PIN_MOTOR_PWM_LEFT, OUTPUT);
   pinMode(PIN_MOTOR_IN0_LEFT, OUTPUT);
@@ -63,8 +54,6 @@ byte i;
 
 long int last;
 
-int count  = 0;
-
 int left_motor_power;
 int left_motor_direction;
 int right_motor_power;
@@ -76,112 +65,107 @@ int right_count = 0;
 double left_coeff = 1;
 double right_coeff = 1;
 
-void calibrate() {
-  left_count = 0;
-  right_count = 0;
-  
-  analogWrite(PIN_MOTOR_PWM_LEFT, 128 * left_coeff);
-  digitalWrite(PIN_MOTOR_IN0_LEFT, HIGH);
-  digitalWrite(PIN_MOTOR_IN1_LEFT , LOW);
-  analogWrite(PIN_MOTOR_PWM_RIGHT, 128 * right_coeff);
-  digitalWrite(PIN_MOTOR_IN0_RIGHT, LOW);
-  digitalWrite(PIN_MOTOR_IN1_RIGHT , HIGH);
-
-  delay(1000);
-  
-  analogWrite(PIN_MOTOR_PWM_LEFT, 0);
-  digitalWrite(PIN_MOTOR_IN0_LEFT, LOW);
-  digitalWrite(PIN_MOTOR_IN1_LEFT , LOW);
-  analogWrite(PIN_MOTOR_PWM_RIGHT, 0);
-  digitalWrite(PIN_MOTOR_IN0_RIGHT, LOW);
-  digitalWrite(PIN_MOTOR_IN1_RIGHT , LOW);
-
-  delay(1000);
-
-  //left_coeff = left_coeff * (right_count / left_count);
-  //right_coeff = right_coeff * (left_count / right_count);
-
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println(left_count);
-  display.println(right_count);
-  display.println(left_coeff);
-  display.println(right_coeff);
-  display.display();
-  
-  delay(1000);
-}
-
-bool calibrated = false;
+char currentChar;
+String command, attribute, data;
+bool newCommand;
 
 void loop() {
-  if (!calibrated && millis() > 10000) {
-    calibrate();
-    calibrate();
-    calibrate();
-    calibrate();
-    calibrate();
-    calibrated = true;
-  }
-  // put your main code here, to run repeatedly:
   display.clearDisplay();
   display.setCursor(0, 0);
   display.println(millis());
+  
   if (Serial.available() > 0) {
-    i = Serial.read();
-    last = millis();
-    
-    left_motor_power = getBit(i, 0);
-    left_motor_direction = getBit(i, 1);
-    right_motor_power = getBit(i, 2);
-    right_motor_direction = getBit(i, 3);
-
-    if (left_motor_power == 1) {
-      analogWrite(PIN_MOTOR_PWM_LEFT, 128 * left_coeff);
-      if (left_motor_direction == 0) {
-        display.print("lf ");
-        digitalWrite(PIN_MOTOR_IN0_LEFT, HIGH);
-        digitalWrite(PIN_MOTOR_IN1_LEFT , LOW);
-      } else if (left_motor_direction == 1) {
-        display.print("lb ");
-        digitalWrite(PIN_MOTOR_IN0_LEFT, LOW);
-        digitalWrite(PIN_MOTOR_IN1_LEFT , HIGH);
+    currentChar = (char)Serial.read();
+    if (currentChar == 2) {
+      command = "";
+      attribute = "";
+      data = "";
+      do {
+        currentChar = (char)Serial.read();
+        if (!isspace(currentChar) && currentChar != -1) {
+          command += currentChar;
+        }
+      } while (!isspace(currentChar));
+      do {
+        currentChar = Serial.read();
+        if (!isspace(currentChar) && currentChar != -1 && currentChar != 3) {
+          attribute += currentChar;
+        }
+      } while (!isspace(currentChar) && currentChar != 3);
+      if (command == "S") {
+        do {
+          currentChar = Serial.read();
+          if (!isspace(currentChar) && currentChar != -1 && currentChar != 3) {
+            data += currentChar;
+          }
+        } while (!isspace(currentChar) && currentChar != 3);
       }
-    } else {
-      display.print("ls ");
-      analogWrite(PIN_MOTOR_PWM_LEFT, 0);
-      digitalWrite(PIN_MOTOR_IN0_LEFT, LOW);
-      digitalWrite(PIN_MOTOR_IN1_LEFT , LOW);
-    }
-
-    if (right_motor_power == 1) {
-      analogWrite(PIN_MOTOR_PWM_RIGHT, 128 * right_coeff);
-      if (right_motor_direction == 0) {
-        display.println("rf");
-        digitalWrite(PIN_MOTOR_IN0_RIGHT, LOW);
-        digitalWrite(PIN_MOTOR_IN1_RIGHT , HIGH);
-      } else if (right_motor_direction == 1) {
-        display.println("rb");
-        digitalWrite(PIN_MOTOR_IN0_RIGHT, HIGH);
-        digitalWrite(PIN_MOTOR_IN1_RIGHT , LOW);
-      }
-    } else {
-      display.println("rs");
-      analogWrite(PIN_MOTOR_PWM_RIGHT, 0);
-      digitalWrite(PIN_MOTOR_IN0_RIGHT, LOW);
-      digitalWrite(PIN_MOTOR_IN1_RIGHT , LOW);
+      do {
+        currentChar = Serial.read();
+      } while (currentChar != 3);
+      newCommand = true;
     }
   }
 
-  display.println(i);
+  if (newCommand) {
+    display.print(command);
+    display.print(" ");
+    display.print(attribute);
+    display.print(" ");
+    display.println(data);
+    if (command == "S") {
+      if (attribute == "GO") {
+        if (data == "F") {
+          analogWrite(PIN_MOTOR_PWM_LEFT, 128 * left_coeff);
+          analogWrite(PIN_MOTOR_PWM_RIGHT, 128 * right_coeff);
+          digitalWrite(PIN_MOTOR_IN0_LEFT, LOW);
+          digitalWrite(PIN_MOTOR_IN1_LEFT , HIGH);
+          digitalWrite(PIN_MOTOR_IN0_RIGHT, HIGH);
+          digitalWrite(PIN_MOTOR_IN1_RIGHT , LOW);
+        } else if (data == "B") {
+          analogWrite(PIN_MOTOR_PWM_LEFT, 128 * left_coeff);
+          analogWrite(PIN_MOTOR_PWM_RIGHT, 128 * right_coeff);
+          digitalWrite(PIN_MOTOR_IN0_LEFT, HIGH);
+          digitalWrite(PIN_MOTOR_IN1_LEFT , LOW);
+          digitalWrite(PIN_MOTOR_IN0_RIGHT, LOW);
+          digitalWrite(PIN_MOTOR_IN1_RIGHT , HIGH);
+        } else if (data == "L") {
+          analogWrite(PIN_MOTOR_PWM_LEFT, 128 * left_coeff);
+          analogWrite(PIN_MOTOR_PWM_RIGHT, 128 * right_coeff);
+          digitalWrite(PIN_MOTOR_IN0_LEFT, HIGH);
+          digitalWrite(PIN_MOTOR_IN1_LEFT , LOW);
+          digitalWrite(PIN_MOTOR_IN0_RIGHT, HIGH);
+          digitalWrite(PIN_MOTOR_IN1_RIGHT , LOW);
+        } else if (data == "R") {
+          analogWrite(PIN_MOTOR_PWM_LEFT, 128 * left_coeff);
+          analogWrite(PIN_MOTOR_PWM_RIGHT, 128 * right_coeff);
+          digitalWrite(PIN_MOTOR_IN0_LEFT, LOW);
+          digitalWrite(PIN_MOTOR_IN1_LEFT , HIGH);
+          digitalWrite(PIN_MOTOR_IN0_RIGHT, LOW);
+          digitalWrite(PIN_MOTOR_IN1_RIGHT , HIGH);
+        } else if (data == "S") {
+          analogWrite(PIN_MOTOR_PWM_LEFT, 0);
+          analogWrite(PIN_MOTOR_PWM_RIGHT, 0);
+          digitalWrite(PIN_MOTOR_IN0_LEFT, LOW);
+          digitalWrite(PIN_MOTOR_IN1_LEFT , LOW);
+          digitalWrite(PIN_MOTOR_IN0_RIGHT, LOW);
+          digitalWrite(PIN_MOTOR_IN1_RIGHT , LOW);
+        }
+      } else if (attribute == "LC") {
+        left_coeff = data.toDouble();
+      } else if (attribute == "RC") {
+        right_coeff = data.toDouble();
+      }
+    }
+  }
 
-  display.println(left_count);
+  display.print(left_coeff);
+  display.print("    ");
+  display.println(right_coeff);
+
+  display.print(left_count);
+  display.print("    ");
   display.println(right_count);
-
-  display.print(digitalRead(PIN_WHEEL_ENCODER_CLK_LEFT));
-  display.print(digitalRead(PIN_WHEEL_ENCODER_DT_LEFT));
-  display.print(digitalRead(PIN_WHEEL_ENCODER_CLK_RIGHT));
-  display.println(digitalRead(PIN_WHEEL_ENCODER_DT_RIGHT));
   
   display.display();
 }
