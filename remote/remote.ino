d5217ac926d463d7aa5b31f5773cf740f387649d
JT Raber <jmann240raber@gmail.com>
2022-05-09 23:02:16 -0400

    final

--- remote/remote.ino
+++ remote/remote.ino
@@ -23,11 +23,11 @@ void setup() {
   pinMode(JX, INPUT);
   pinMode(JY, INPUT);
 
-  pinMode(SW1, INPUT_PULLUP);
-  pinMode(SW2, INPUT_PULLUP);
-  pinMode(SW3, INPUT_PULLUP);
-  pinMode(SW4, INPUT_PULLUP);
-  pinMode(SW5, INPUT_PULLUP);
+  pinMode(SW1, INPUT_PULLUP); // Left wheel adjust
+  pinMode(SW2, INPUT_PULLUP); // Toggle menu
+  pinMode(SW3, INPUT_PULLUP); // Right wheel adjust
+  pinMode(SW4, INPUT_PULLUP); // Wheel adjust up
+  pinMode(SW5, INPUT_PULLUP); // Wheel adjust down
   pinMode(SW6, INPUT_PULLUP);
   pinMode(SW7, INPUT_PULLUP);
 
@@ -52,22 +52,50 @@ long int last_time;
 
 int forward, leftward;
 
+bool menu_button_state = false;
+bool menu_button_state_last = false;
+
+bool show_path = false;
+
+char currentChar;
+String command, attribute, data;
+bool newCommand;
+
+bool path[64][32];
+
+int robot_x = 32;
+int robot_y = 16;
+double robot_r = 0;
+
 void printVars() {
   display.clearDisplay();
   display.setCursor(0, 0);
-  display.print("time: ");
   display.println(current_time);
-  display.print("right_coeff: ");
+  display.print(left_coeff);
+  display.print(" ");
   display.println(right_coeff);
-  display.print("left_coeff: ");
-  display.println(left_coeff);
-  display.print(digitalRead(SW1));
-  display.print(" ");
-  display.println(digitalRead(SW3));
-  display.print(" ");
-  display.println(digitalRead(SW4));
-  display.print(" ");
-  display.println(digitalRead(SW5));
+  display.print("(");
+  display.print(robot_x);
+  display.print(", ");
+  display.print(robot_y);
+  display.print("), ");
+  display.println(robot_r);
+  display.display();
+}
+
+void drawPath() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  for (int x = 0; x < 64; x++) {
+    for (int y = 0; y < 32; y++) {
+      if (path[x][y]) {
+        display.drawPixel(x*2, 63-y*2, SSD1306_WHITE);
+        display.drawPixel(x*2+1, 63-y*2, SSD1306_WHITE);
+        display.drawPixel(x*2+1, 62-y*2, SSD1306_WHITE);
+        display.drawPixel(x*2, 62-y*2, SSD1306_WHITE);
+      }
+    }
+  }
   display.display();
 }
 
@@ -94,6 +122,14 @@ void loop() {
       right_coeff -= 0.01;
     }
   }
+
+  menu_button_state = digitalRead(SW2);
+
+  if (menu_button_state == 0 && menu_button_state_last == 1) {
+    show_path = !show_path;
+  }
+
+  menu_button_state_last = menu_button_state;
 
   if (current_time >= last_time + bt_msg_stride) {
     if (forward > (1024 - 256)) {
@@ -136,7 +172,67 @@ void loop() {
     last_time = current_time;
   }
 
-  printVars();
+
+
+  if (Serial1.available() > 0) {
+    currentChar = (char)Serial1.read();
+    if (currentChar == 2) {
+      command = "";
+      attribute = "";
+      data = "";
+      do {
+        currentChar = (char)Serial1.read();
+        if (!isspace(currentChar) && currentChar != -1) {
+          command += currentChar;
+        }
+      } while (!isspace(currentChar));
+      do {
+        currentChar = Serial1.read();
+        if (!isspace(currentChar) && currentChar != -1 && currentChar != 3) {
+          attribute += currentChar;
+        }
+      } while (!isspace(currentChar) && currentChar != 3);
+      if (command == "S") {
+        do {
+          currentChar = Serial1.read();
+          if (!isspace(currentChar) && currentChar != -1 && currentChar != 3) {
+            data += currentChar;
+          }
+        } while (!isspace(currentChar) && currentChar != 3);
+      }
+      do {
+        currentChar = Serial1.read();
+      } while (currentChar != 3);
+      newCommand = true;
+    }
+  }
+
+  if (newCommand) {
+    display.print(command);
+    display.print(" ");
+    display.print(attribute);
+    display.print(" ");
+    display.println(data);
+    if (command == "S") {
+      if (attribute == "R") {
+        robot_r = data.toDouble();
+      } else if (attribute == "X") {
+        robot_x = data.toInt();
+        path[robot_x][robot_y] = true;
+      } else if (attribute == "Y") {
+        robot_y = data.toInt();
+        path[robot_x][robot_y] = true;
+      }
+    }
+  }
+
+  
+
+  if (show_path) {
+    drawPath();
+  } else {
+    printVars();
+  }
 
   delay(10);
 }
--- robot/robot.ino
+++ robot/robot.ino
@@ -1,43 +1,98 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+
+// Motor pins
 
 #define PIN_MOTOR_PWM_LEFT    9
 #define PIN_MOTOR_IN0_LEFT    7
 #define PIN_MOTOR_IN1_LEFT    8
-
 #define PIN_MOTOR_PWM_RIGHT    4
 #define PIN_MOTOR_IN0_RIGHT    6
 #define PIN_MOTOR_IN1_RIGHT    5
 
+
+// Rotary encoder pins
+
 #define PIN_WHEEL_ENCODER_CLK_LEFT    2
 #define PIN_WHEEL_ENCODER_DT_LEFT    A3
-
 #define PIN_WHEEL_ENCODER_CLK_RIGHT    3
 #define PIN_WHEEL_ENCODER_DT_RIGHT    A1
 
+
+// Device initialization
+
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
+Adafruit_MPU6050 mpu;
+
+
+// Time
+
+int current_millis = 0, last_millis = 0;
+float delta_millis = 0;
+
+float rate = 100; //Hz
+
+
+// MPU
+
+sensors_event_t a, g, temp;
+float baseline_gyro_z;
+float rot = 0;
+
+
+// Odometer/wheels
+
+int left_count = 0;
+int right_count = 0;
+
+int last_left_count = 0;
+int last_right_count = 0;
+
+int delta_left_count = 0;
+int delta_right_count = 0;
+
+float left_coeff = 1;
+float right_coeff = 1;
+
+double robot_x = 32;
+double robot_y = 16;
+
+double scale = 0.01;
+
+
+//Commands
+
+char currentChar;
+String command, attribute, data;
+bool newCommand;
+
 
 void setup() {
+  // Begin the serial
   Serial.begin(38400, SERIAL_8E2);
-  
+
+  // Set the motor pins as outputs
   pinMode(PIN_MOTOR_PWM_LEFT, OUTPUT);
   pinMode(PIN_MOTOR_IN0_LEFT, OUTPUT);
   pinMode(PIN_MOTOR_IN1_LEFT, OUTPUT);
-  
   pinMode(PIN_MOTOR_PWM_RIGHT, OUTPUT);
   pinMode(PIN_MOTOR_IN0_RIGHT, OUTPUT);
   pinMode(PIN_MOTOR_IN1_RIGHT, OUTPUT);
-  
+
+  // Set the encoder pins as inputs
   pinMode(PIN_WHEEL_ENCODER_DT_LEFT, INPUT_PULLUP);
   pinMode(PIN_WHEEL_ENCODER_CLK_LEFT, INPUT_PULLUP);
-  
   pinMode(PIN_WHEEL_ENCODER_DT_RIGHT, INPUT_PULLUP);
   pinMode(PIN_WHEEL_ENCODER_CLK_RIGHT, INPUT_PULLUP);
-  
+
+  // Attach the counter interrupts to the encoder clock pins
   attachInterrupt(digitalPinToInterrupt(PIN_WHEEL_ENCODER_CLK_RIGHT), updateEncoderRight, CHANGE);  
   attachInterrupt(digitalPinToInterrupt(PIN_WHEEL_ENCODER_CLK_LEFT), updateEncoderLeft, CHANGE);  
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+  // Initialize the display and clear it
+  /*if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for (;;); // Don't proceed, loop forever
   }
@@ -45,105 +100,155 @@ void setup() {
   display.clearDisplay();
   display.setTextColor(WHITE);
   display.setTextSize(1);
-  display.setCursor(0, 0);
+  display.setCursor(0, 0);*/
+
+  // Initialize the MPU
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  mpu.getEvent(&a, &g, &temp);
+  baseline_gyro_z = g.gyro.z;
 
   delay(1000);
 }
 
-byte i;
-
-long int last;
-
-int left_motor_power;
-int left_motor_direction;
-int right_motor_power;
-int right_motor_direction;
-
-int left_count = 0;
-int right_count = 0;
-
-double left_coeff = 1;
-double right_coeff = 1;
-
-char currentChar;
-String command, attribute, data;
-bool newCommand;
 
 void loop() {
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println(millis());
+  // Set the current and last time
+  last_millis = current_millis;
+  current_millis = millis();
+  delta_millis = current_millis - last_millis;
+
+  // Set the last and delta counts
+  delta_left_count = left_count - last_left_count;
+  delta_right_count = right_count - last_right_count;
+  last_left_count = left_count;
+  last_right_count = right_count;
+
+  // Read from the MPU and set the rot
+  mpu.getEvent(&a, &g, &temp);
+  rot += ((g.gyro.z - baseline_gyro_z) / (1000 / delta_millis));
+
+  //Calculate the change in X and Y
+  robot_x += (scale * cos(rot) * (delta_left_count + delta_right_count) / 2);
+  robot_y += (scale * sin(rot) * (delta_left_count + delta_right_count) / 2);
   
+  // Reset the display and print milliseconds
+  /*display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println(millis());*/
+
+  // Read serial command if it is available  
   if (Serial.available() > 0) {
+
+    // Read the first character
     currentChar = (char)Serial.read();
+
+    // If it isn't a 2, ignore it
     if (currentChar == 2) {
+
+      // Empty the command, attribute, and data variables
       command = "";
       attribute = "";
       data = "";
+
+      // Create the command
       do {
+        // Read a character and add it to the command if it is valid
         currentChar = (char)Serial.read();
         if (!isspace(currentChar) && currentChar != -1) {
           command += currentChar;
         }
+
+        // Read again if we haven't reached a space
       } while (!isspace(currentChar));
+
+      //Create the attribute
       do {
+        // Read a character and add it to the attribute if it is valid
         currentChar = Serial.read();
         if (!isspace(currentChar) && currentChar != -1 && currentChar != 3) {
           attribute += currentChar;
         }
+
+        // Read again if we haven't reached a space
       } while (!isspace(currentChar) && currentChar != 3);
+
+      // If the command was to set the attribute
       if (command == "S") {
+
+        // Create the data
         do {
+          // Read a character and add it to the data if it is valid
           currentChar = Serial.read();
           if (!isspace(currentChar) && currentChar != -1 && currentChar != 3) {
             data += currentChar;
           }
+
+        // Read again if we haven't reached a space
         } while (!isspace(currentChar) && currentChar != 3);
       }
+
+      // Clear the serial buffer if we have some leftovers
       do {
         currentChar = Serial.read();
       } while (currentChar != 3);
+
+      // Let the rest of the program know we have a new command to process
       newCommand = true;
     }
   }
-
+  
+  // If there is a new command to process
   if (newCommand) {
-    display.print(command);
-    display.print(" ");
-    display.print(attribute);
-    display.print(" ");
-    display.println(data);
+
+    // Print it
+    /*Serial.print(command);
+    Serial.print(" ");
+    Serial.print(attribute);
+    Serial.print(" ");
+    Serial.println(data);*/
+
+    // If it is a set command
     if (command == "S") {
-      if (attribute == "GO") {
-        if (data == "F") {
+      if (attribute == "GO") { // Motor control
+        if (data == "F") { // Forward
           analogWrite(PIN_MOTOR_PWM_LEFT, 128 * left_coeff);
           analogWrite(PIN_MOTOR_PWM_RIGHT, 128 * right_coeff);
           digitalWrite(PIN_MOTOR_IN0_LEFT, LOW);
           digitalWrite(PIN_MOTOR_IN1_LEFT , HIGH);
           digitalWrite(PIN_MOTOR_IN0_RIGHT, HIGH);
           digitalWrite(PIN_MOTOR_IN1_RIGHT , LOW);
-        } else if (data == "B") {
+        } else if (data == "B") { // Backward
           analogWrite(PIN_MOTOR_PWM_LEFT, 128 * left_coeff);
           analogWrite(PIN_MOTOR_PWM_RIGHT, 128 * right_coeff);
           digitalWrite(PIN_MOTOR_IN0_LEFT, HIGH);
           digitalWrite(PIN_MOTOR_IN1_LEFT , LOW);
           digitalWrite(PIN_MOTOR_IN0_RIGHT, LOW);
           digitalWrite(PIN_MOTOR_IN1_RIGHT , HIGH);
-        } else if (data == "L") {
+        } else if (data == "L") { // Leftward
           analogWrite(PIN_MOTOR_PWM_LEFT, 128 * left_coeff);
           analogWrite(PIN_MOTOR_PWM_RIGHT, 128 * right_coeff);
           digitalWrite(PIN_MOTOR_IN0_LEFT, HIGH);
           digitalWrite(PIN_MOTOR_IN1_LEFT , LOW);
           digitalWrite(PIN_MOTOR_IN0_RIGHT, HIGH);
           digitalWrite(PIN_MOTOR_IN1_RIGHT , LOW);
-        } else if (data == "R") {
+        } else if (data == "R") { // Rightward
           analogWrite(PIN_MOTOR_PWM_LEFT, 128 * left_coeff);
           analogWrite(PIN_MOTOR_PWM_RIGHT, 128 * right_coeff);
           digitalWrite(PIN_MOTOR_IN0_LEFT, LOW);
           digitalWrite(PIN_MOTOR_IN1_LEFT , HIGH);
           digitalWrite(PIN_MOTOR_IN0_RIGHT, LOW);
           digitalWrite(PIN_MOTOR_IN1_RIGHT , HIGH);
-        } else if (data == "S") {
+        } else if (data == "S") { // Stop
           analogWrite(PIN_MOTOR_PWM_LEFT, 0);
           analogWrite(PIN_MOTOR_PWM_RIGHT, 0);
           digitalWrite(PIN_MOTOR_IN0_LEFT, LOW);
@@ -151,23 +256,30 @@ void loop() {
           digitalWrite(PIN_MOTOR_IN0_RIGHT, LOW);
           digitalWrite(PIN_MOTOR_IN1_RIGHT , LOW);
         }
-      } else if (attribute == "LC") {
+      } else if (attribute == "LC") { // Left coefficient
         left_coeff = data.toDouble();
-      } else if (attribute == "RC") {
+      } else if (attribute == "RC") { // Right coefficient
         right_coeff = data.toDouble();
       }
     }
   }
 
-  display.print(left_coeff);
-  display.print("    ");
-  display.println(right_coeff);
+  Serial.write(2);
+  Serial.print("S R ");
+  Serial.print(rot);
+  Serial.write(3);
 
-  display.print(left_count);
-  display.print("    ");
-  display.println(right_count);
-  
-  display.display();
+  Serial.write(2);
+  Serial.print("S X ");
+  Serial.print(robot_x);
+  Serial.write(3);
+
+  Serial.write(2);
+  Serial.print("S Y ");
+  Serial.print(robot_y);
+  Serial.write(3);
+
+  delay(1000/rate);
 }
 
 void updateEncoderLeft(){
@@ -176,7 +288,7 @@ void updateEncoderLeft(){
 
   if (encoderPin_X == 0){   
     if (encoderPin_Y == 0){
-      left_count++;
+      left_count--;
     }else{
       left_count++;
     }
@@ -184,7 +296,7 @@ void updateEncoderLeft(){
     if (encoderPin_Y == 0){
       left_count++;
     }else{
-      left_count++;
+      left_count--;
     }    
   }
 }
